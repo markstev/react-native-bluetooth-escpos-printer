aec483b64fcea315fd3edf3ed113ccad229f5d58
Mark Stevens <stevens.mark.a@gmail.com>
2019-08-17 14:19:44 -0700

    Fixed androidx bug

--- android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
@@ -10,7 +10,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import androidx.core.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
